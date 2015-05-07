ea98b0393554ce0d733b4b65e84238704f8e133f
tfree87 <tfree87@users.noreply.github.com>
2015-05-07 17:44:57 -0400

    Added '__main__' function
    Consolidated gnucash2ledger into more functions to make code reading easier. Added a header for Emacs so that Emacs users will get pretty print text.

--- gnucash2ledger.py
+++ gnucash2ledger.py
@@ -3,17 +3,11 @@ import sys
 import codecs
 import gnucash
 
-out = codecs.getwriter('UTF-8')(sys.stdout)
-if len(sys.argv) == 1:
-	sys.stderr.write("Invocation: %s gnucash_filename\n" % sys.argv[0])
-	sys.exit(1)
-data = gnucash.read_file(sys.argv[1])
-
 def format_commodity(commodity):
 	mnemonic = commodity.mnemonic
 	try:
 		if mnemonic.encode('ascii').isalpha():
-			if mnemonic == "USD":
+                        if mnemonic == "USD":
                                 return "$"
                         else:
                                 return mnemonic
@@ -28,59 +22,76 @@ def full_acc_name(acc):
 	result += acc.name
 	return result
 
-commodities = data.commodities.values()
-for commodity in commodities:
-	if commodity.mnemonic == "":
-		continue
-	out.write("commodity %s\n" % format_commodity(commodity))
-	if commodity.fullname != "":
-		out.write("\tnote %s\n" % commodity.fullname)
-out.write("\n")
+def write_commodities():
+        commodities = data.commodities.values()
+        for commodity in commodities:
+                if commodity.mnemonic == "":
+                        continue
+                out.write("commodity %s\n" % format_commodity(commodity))
+                if commodity.fullname != "":
+                        out.write("\tnote %s\n" % commodity.fullname)
+        out.write("\n")
 
-accounts = data.accounts.values()
-for acc in accounts:
-	# ignore "dummy" accounts
-	if acc.type is None or acc.type == "ROOT":
-		continue
-	if str(acc.commodity) == "template":
-		continue
-	out.write("account %s\n" % (full_acc_name(acc), ))
-	if acc.description != "":
-		out.write("\tnote %s\n" % (acc.description,))
-	formated_commodity = format_commodity(acc.commodity)
-	formated_commodity = formated_commodity.replace("\"", "\\\"")
-	out.write("\tcheck commodity == \"%s\"\n" % formated_commodity)
-	out.write("\n")
+def write_accounts():
+        accounts = data.accounts.values()
+        for acc in accounts:
+                # ignore "dummy" accounts
+                if acc.type is None or acc.type == "ROOT":
+                        continue
+                if str(acc.commodity) == "template":
+                        continue
+                out.write("account %s\n" % (full_acc_name(acc), ))
+                if acc.description != "":
+                        out.write("\tnote %s\n" % (acc.description,))
+                formated_commodity = format_commodity(acc.commodity)
+                formated_commodity = formated_commodity.replace("\"", "\\\"")
+                out.write("\tcheck commodity == \"%s\"\n" % formated_commodity)
+                out.write("\n")
 
 # Prices
-prices = data.prices.values()
-prices.sort(key = lambda x: x.date)
-for price in prices:
-	date = price.date.strftime("%Y/%m/%d %H:%M:%S")
-	out.write("P %s %s %s %s\n" % (date, format_commodity(price.commodity), price.value, format_commodity(price.currency)))
-out.write("\n")
+def write_prices():
+        prices = data.prices.values()
+        prices.sort(key = lambda x: x.date)
+        for price in prices:
+                date = price.date.strftime("%Y-%m-%d %H:%M:%S")
+                out.write("P %s %s %s %s\n" % (date, format_commodity(price.commodity), price.value, format_commodity(price.currency)))
+        out.write("\n")
 
-transactions = data.transactions.values()
-transactions.sort(key=lambda x: x.post_date)
-for trans in transactions:
-	date = trans.post_date.strftime("%Y/%m/%d")
-	out.write("%s (%s) %s\n" % (date, trans.num, trans.description))
-	for split in trans.splits:
-		if split.reconcile_state == "y":
-                        state="*"
-                elif split.reconcile_state == "c":
-                        state="!"
-                else:
-                        state=""
-		out.write("\t%s %-40s " % (state,full_acc_name(split.account)))
-		if split.account.commodity != trans.currency:
-                        if format_commodity(trans.currency) == "$":
-                                out.write("\t{0:f} {1} @@ ${2:0.2f}\t; {3}".format(split.quantity, format_commodity(split.account.commodity), abs(split.value), split.memo))
+
+def write_transactions():
+        transactions = data.transactions.values()
+        transactions.sort(key=lambda x: x.post_date)
+        for trans in transactions:
+                date = trans.post_date.strftime("%Y/%m/%d")
+                out.write("%s (%s) %s\n" % (date, trans.num, trans.description))
+                for split in trans.splits:
+                        if split.reconcile_state == "y":
+                                state="*"
+                        elif split.reconcile_state == "c":
+                                state="!"
                         else:
-                                out.write("\t{0:f} {1} @@ {2:0.2f} {3}\t; {4}".format(split.quantity, format_commodity(split.account.commodity), abs(split.value), format_commodity(trans.currency), split.memo))
-                elif format_commodity(split.account.commodity) == "$":
-                        out.write("\t${0:0.2f}\t; {1}".format(split.value, split.memo))
-                else:
-                	out.write("{0:0.2f} {1}\t; {2}".format(split.value, format_commodity(trans.currency), split.memo))
-		out.write("\n")
-	out.write("\n")
+                                state=""
+                        out.write("\t%s %-40s " % (state, full_acc_name(split.account)))
+                        if split.account.commodity != trans.currency:
+                                if format_commodity(trans.currency) == "$":
+                                        out.write("\t{:f} {} @@ {}{:0.2f}\t; {}".format(split.quantity, format_commodity(split.account.commodity), format_commodity(trans.currency), abs(split.value), split.memo))
+                                else:
+                                        out.write("\t{:f} {} @@ {:0.2f} {}\t; {}".format(split.quantity, format_commodity(split.account.commodity), abs(split.value), format_commodity(trans.currency), split.memo))
+                        elif format_commodity(split.account.commodity) == "$":
+                                out.write("\t{}{:0.2f}\t; {}".format(format_commodity(trans.currency), split.value, split.memo))
+                        else:
+                                out.write("{0:0.2f} {1}\t; {2}".format(split.value, format_commodity(trans.currency), split.memo))
+                        out.write("\n")
+                out.write("\n")
+    
+if __name__ == '__main__':
+        out = codecs.getwriter('UTF-8')(sys.stdout)
+        if len(sys.argv) == 1:
+                sys.stderr.write("Invocation: {} gnucash_filename\n".format(sys.argv[0]))
+                sys.exit(1)
+        data = gnucash.read_file(sys.argv[1])
+        out.write(";; -*- mode: ledger; -*-\n\n")
+        write_commodities()
+        write_accounts()
+        write_prices()
+        write_transactions()
